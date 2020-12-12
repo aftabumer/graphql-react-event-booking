09a559ec565ef38e5c89c081e265ddccae3afaf4
aftabumer <innocentboy0300@gmail.com>
2020-12-12 21:42:01 +0500

    -setup mongodb and graphql. -create schema for the add event. -add event into mongodb colloction.

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ const graphqlHttp = require("express-graphql").graphqlHTTP;
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
 
+const Event = require("./models/event");
+
 const app = express();
 
-const events = [];
-
 app.use(bodyParser.json());
-
-// app.get("/", (req, res, next) => {
-//   res.send("Hello World");
-// });
 
 app.use(
   "/graphql",
@@ -46,19 +42,35 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc };
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
       },
       createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          //   date: new Date().toISOString(),
-          date: args.eventInput.date,
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date),
+        });
+
+        return event
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc };
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
       },
     },
     graphiql: true,
@@ -67,7 +79,7 @@ app.use(
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.mhn2x.mongodb.net/<dbname>?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.mhn2x.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => app.listen(3000))
   .catch((err) => console.log(err));
