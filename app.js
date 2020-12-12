60e342ea149def96457d3f5cda3d746c17decc6c
aftabumer <innocentboy0300@gmail.com>
2020-12-12 16:51:52 +0500

    -set type and date for our event schema.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 // app.get("/", (req, res, next) => {
@@ -15,11 +17,26 @@ app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
     type RootQuery {
-        events: [String!]!
+        events: [Event!]!
     }
     type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
     }
     schema {
         query: RootQuery
@@ -28,11 +45,19 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ["Romentic Cooking", "Seailing", "All night coding"];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          //   date: new Date().toISOString(),
+          date: args.eventInput.date,
+        };
+        events.push(event);
+        return event;
       },
     },
     graphiql: true,
