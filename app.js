5cd47b0c831657b69dbaff4dd313ef2d8315e3b6
aftabumer <innocentboy0300@gmail.com>
2020-12-12 17:45:35 +0500

    -setup mongoose in our app. -connect app.js with mongoose project.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql").graphqlHTTP;
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -64,4 +65,9 @@ app.use(
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.mhn2x.mongodb.net/<dbname>?retryWrites=true&w=majority`
+  )
+  .then(() => app.listen(3000))
+  .catch((err) => console.log(err));
